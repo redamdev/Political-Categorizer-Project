c1fb9fd6c5cc2a1aae328096a91a7e219256bf59
nermeenrizk740 <123003752+nermeenrizk740@users.noreply.github.com>
2024-05-16 19:13:37 -0400

    Update scrapingNewsPaper.py

--- scrapingNewsPaper.py
+++ scrapingNewsPaper.py
@@ -18,6 +18,33 @@ def scrapeNewsArticleLink(url):
         #parsing the content of the html
         soup = BeautifulSoup(response.content, 'html.parser')
 
+        #Extract the title
+        titleTag = soup.find('title')
+        if titleTag:
+            title = titleTag.string.strip()
+        else:
+            'no title found'
+
+        
+        #Extract the date
+        dateTag = soup.find('meta', attrs={'property': 'article:published_time'})
+        if dateTag:
+            date = dateTag['content'].strip()
+        else:
+            'no Date'
+        
+
+        #Extract the publisher
+        publisherTag = soup.find('meta', attrs={'name': 'publisher'})
+        if publisherTag:
+            publisher = publisherTag['content'].strip()
+        else:
+            'no Publisher name found in the aticle'
+            
+
+        #Extract the bode
+        bodyTag = soup.find_all('p')
+        body = body = ' '.join([p.get_text().strip() for p in bodyTag])
 
 
 
