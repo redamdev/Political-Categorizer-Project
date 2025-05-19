a653a525788e87ec0ec0370ec44e2e3f4b9cb59a
Reda Majid <148282750+redamdev@users.noreply.github.com>
2025-05-19 18:58:07 -0400

    Update firebase.js

--- NewsBuster v2/frontend/firebase.js
+++ NewsBuster v2/frontend/firebase.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC893euNJkE01PP5r4GbUGZ2UUUKihkjHM",
-  authDomain: "newsbustr.firebaseapp.com",
-  projectId: "newsbustr",
-  storageBucket: "newsbustr.appspot.com",
-  messagingSenderId: "968689701636",
-  appId: "1:968689701636:web:417fac247403f544ae3651",
-  measurementId: "G-3HYNMEJEH3",
-};
+// Firebase Config Here
+// const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
